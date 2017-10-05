a722047a2b0c41c2d480023af981ac3cc03e203f
the-daniel-rothig <daniel.rothig@gmail.com>
2017-10-05 15:34:45 +0100

    conf transf

--- docker-compose-transform.sh
+++ docker-compose-transform.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 's/replacewebimage/'"$1"'/g'
-sed -i 's/replacewebimage/'"$1"'/g' docker-compose.yml
+sed -i 's/replacewebimage/registry20171003093523\.azurecr\.io\/the-daniel-rothig\/dnettest:'"$1"'/g' docker-compose.yml
